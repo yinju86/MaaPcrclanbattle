4206c7ca5d835b82c37cdf376b20c070a4036b65
sy y <s86469525@gmail.com>
2024-09-29 02:14:28 +0800

    初版本

--- install.py
+++ install.py
@@ -49,6 +49,18 @@ def install_resource():
         working_dir / "assets" / "interface.json",
         install_path,
     )
+    shutil.copy2(
+        working_dir / "assets" / "generator.py",
+        install_path,
+    )
+    shutil.copy2(
+        working_dir / "assets" / "generator.exe",
+        install_path,
+    )
+    shutil.copy2(
+        working_dir / "assets" / "scriptgeneration.py",
+        install_path,
+    )
 
     with open(install_path / "interface.json", "r", encoding="utf-8") as f:
         interface = json.load(f)
