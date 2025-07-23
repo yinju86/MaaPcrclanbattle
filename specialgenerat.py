eb882e740effa4981dd496009d69bc9d64264abf
yinju86 <s86469525@gmail.com>
2025-07-23 21:55:26 +0800

    浼樺寲鍗″抚閫昏緫2

--- specialgenerat.py
+++ specialgenerat.py
@@ -236,12 +236,12 @@ def generation(stepname,stepfile,namelist,speed=200):
         elif c == 'k':
             actions.append(f'''"{stepname}{j}": 
                            {{ "recognition": "OCR", "threshold": 0.75, 
-                           "action": "Click", "expected": ["卡帧"], "pre_delay": {speed}, "post_delay": {speed}, "rate_limit": 100, "timeout": 5000000,
+                           "action": "Click", "expected": ["卡帧|菜单"], "pre_delay": {speed}, "post_delay": {speed}, "rate_limit": 100, "timeout": 5000000,
                             "next": ["{stepname}{j}_e"] 
                            }},''')
             actions.append(f'''"{stepname}{j}_e": 
                            {{ "recognition": "OCR", "threshold": 0.75, "focus": true, "focus_tip": ["自行目压,目押完毕点击 设定"], 
-                           "action": "Click", "expected": ["卡帧"], "pre_delay": {speed}, "post_delay": {speed}, "rate_limit": 100, "timeout": 5000000,
+                           "action": "Click", "expected": ["卡帧|菜单"], "pre_delay": {speed}, "post_delay": {speed}, "rate_limit": 100, "timeout": 5000000,
                             "next": ["{stepname}{j}gb"] 
                            }},''')
             actions.append(f'"{stepname}{j}gb": {{ "recognition": "TemplateMatch", "pre_delay": {speed}, "post_delay": {speed},"template": ["gb.png"], "action": "Click", "next": ["{stepname}{j}done"] }},')
